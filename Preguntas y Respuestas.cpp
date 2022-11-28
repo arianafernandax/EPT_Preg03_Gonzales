b7e090524ebaadc5989ef99ca35d71bcee826226
FerSo22 <20a301073@untels.edu.pe>
2022-11-27 21:38:45 -0500

    Evaluación subida

---
+++ Preguntas y Respuestas.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main(){
+	setlocale(LC_CTYPE, "Spanish");
+	
+	cout<< "*********BIENVENIDOS A SU PRACTICA CALIFICADA************\n";
+	cout<< "\nPregunta 01: La casa de los Perez tiene 3 pisos.\nSi se sabe que el cuadrado de la diferencia de altura y de cinco es 16.\nHallar dicha altura si se sabe que es mayor a 3. ";
+	cout<< "\n\nRespuesta: \n\n";
+	system("pause");
+	cout<<"-----------------------------------------------------------------------------------------------------";
+	cout<< "\nPregunta 02: Un automóvil robado que escapa de la policía a 17 m/s \nse estrella al cabo de 2s en la casa de los Perez. \nTeniendo en cuenta que dicho automovil viajaba a una velocidad constante\nantes del choque \nhalle la distancia desde el punto A hasta el punto B.";
+	cout<< "\n\nRespuesta: \n\n";
+	system("pause");
+	cout<<"-----------------------------------------------------------------------------------------------------";
+	cout<< "\nPregunta 03: Un Drone policial se encuentra en el punto C \ny se desplaza hasta el punto D para perseguir al automovil robado.\nSe tiene como dato que la distancia entre el Drone y el auto chocado es de 15 metros.\nHallar la distancia recorrida por el Drone teniendo en cuenta que el Drone y el auto\nparten de un mismo punto a diferentes alturas. ";
+	cout<< "\n\nRespuesta:  \n\n";
+	system("pause");
+	cout<<"-----------------------------------------------------------------------------------------------------";
+	cout<< "\nPregunta 04: Calcular el área del trapecio formado por \nlos puntos A,B,C y D en dicha gráfica. ";
+	cout<< "\n\nRespuesta:  \n\n";
+	system("pause");
+	
+	
+	return 0;
+}
