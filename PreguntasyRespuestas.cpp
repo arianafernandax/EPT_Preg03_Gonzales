0d549fef0ece55f055845406860d712e63f376fa
joshua flores <fjoshua4d20@feyalegria17.edu.pe>
2022-11-28 12:00:36 -0500

    Flores_Casavilca

--- PreguntasyRespuestas.cpp
+++ PreguntasyRespuestas.cpp
@@ -12,7 +12,7 @@ int main(){
 	system("pause");
 	cout<<"-----------------------------------------------------------------------------------------------------";
 	cout<< "\nPregunta 02: Un automóvil robado que escapa de la policía a 17 m/s \nse estrella al cabo de 2s en la casa de los Perez. \nTeniendo en cuenta que dicho automovil viajaba a una velocidad constante\nantes del choque \nhalle la distancia desde el punto A hasta el punto B.";
-	cout<< "\n\nRespuesta: \n\n";
+	cout<< "\n\nRespuesta: 34\n\n";
 	system("pause");
 	cout<<"-----------------------------------------------------------------------------------------------------";
 	cout<< "\nPregunta 03: Un Drone policial se encuentra en el punto C \ny se desplaza hasta el punto D para perseguir al automovil robado.\nSe tiene como dato que la distancia entre el Drone y el auto chocado es de 15 metros.\nHallar la distancia recorrida por el Drone teniendo en cuenta que el Drone y el auto\nparten de un mismo punto a diferentes alturas. ";
